cea2b16cc0f9453830c03d780ded8b157f845f41
shubhammehta1003 <84181294+shubhammehta1003@users.noreply.github.com>
2021-10-12 17:45:46 +0530

    Add files via upload

---
+++ Tokens and operators part 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Code to find multiplication of two numbers//
+    
+    int a,b;
+    
+    cout<<"Enter the first number"<<endl;
+    cin>>a;
+    
+    cout<<"Enter the second number"<<endl;
+    cin>>b;
+    
+    int Product= a*b;
+    cout<<"Product="<<Product;
+
+    return 0;
+}
